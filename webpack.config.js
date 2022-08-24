87bd34d6bfa015d3eedb2ee62a7c615a4af725da
pbatsaihan <batsaihan.p@gmail.com>
2022-08-24 16:08:03 +0800

    Create Docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,11 @@ module.exports = {
   mode: "development",
   entry: "./src/js/index.js",
   devServer: {
-    static: "./dist",
+    static: "./docs",
   },
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "docs"),
   },
   plugins: [
     new HtmlWebpackPlugin({
